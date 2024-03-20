fd58bc46850c59e8879ecdfe3e00bb5068907754
Rakshitha <27rakshitha@gmail.com>
2024-03-20 21:44:20 +0530

    Adding objects to local Storage

---
+++ local Storage/index.js
@@ -0,0 +1,18 @@
+function handleFormSubmit(event) {
+  event.preventDefault()
+
+  const name = event.target.username.value
+  const mail = event.target.email.value
+  const phone = event.target.phone.value
+
+  let myObj = {
+    Username: name,
+    Email: mail,
+    Phone: phone
+  }
+
+  let jsonObj = JSON.stringify(myObj)
+  localStorage.setItem('User Details', jsonObj)
+  
+}
+//module.exports = handleFormSubmit
